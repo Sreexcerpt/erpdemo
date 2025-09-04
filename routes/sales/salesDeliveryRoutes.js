91121db6c137989ee8874ef91f63ee6339c1a5c0
Sreexcerpt <sree.excerpt@gmail.com>
2025-09-04 18:40:56 +0530

    7th

--- routes/sales/salesDeliveryRoutes.js
+++ routes/sales/salesDeliveryRoutes.js
@@ -6,15 +6,15 @@ const salesDeliveryController = require('../../controllers/sales/salesDeliveryCo
 router.post('/generate-delivery-number', salesDeliveryController.generateDeliveryNumber);
 
 // CRUD operations
-router.post('/sales-deliveries', salesDeliveryController.createSalesDelivery);
-router.get('/sales-deliveries', salesDeliveryController.getAllSalesDeliveries);
-router.get('/sales-deliveries/:id', salesDeliveryController.getSalesDeliveryById);
-router.put('/sales-deliveries/:id', salesDeliveryController.updateSalesDelivery);
-router.delete('/sales-deliveries/:id', salesDeliveryController.deleteSalesDelivery);
+router.post('/salesdeliveries', salesDeliveryController.createSalesDelivery);
+router.get('/salesdeliveries', salesDeliveryController.getAllSalesDeliveries);
+router.get('/salesdeliveries/:id', salesDeliveryController.getSalesDeliveryById);
+router.put('/salesdeliveries/:id', salesDeliveryController.updateSalesDelivery);
+router.delete('/salesdeliveries/:id', salesDeliveryController.deleteSalesDelivery);
 
 // Special operations
-router.get('/sales-deliveries/by-order/:salesOrderId', salesDeliveryController.getDeliveriesBySalesOrder);
-router.patch('/sales-deliveries/:id/status', salesDeliveryController.updateDeliveryStatus);
+router.get('/salesdeliveries/by-order/:salesOrderId', salesDeliveryController.getDeliveriesBySalesOrder);
+router.patch('/salesdeliveries/:id/status', salesDeliveryController.updateDeliveryStatus);
 router.get('/delivery-statistics', salesDeliveryController.getDeliveryStatistics);
 
 module.exports = router;
